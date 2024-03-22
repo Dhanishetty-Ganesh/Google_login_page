bbec09152cd3c7f9978e8822c68ab776b60697f0
Dhanishetty-Ganesh <varmaganesh224@gmail.com>
2024-03-23 00:34:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import {useAuth0} from '@auth0/auth0-react'
+import Home from './components/Home';
 
+import './App.css';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const { user, loginWithRedirect,isAuthenticated,logout } = useAuth0();
+    console.log('Current User',user)
+    return (
+      <div className="App">
+          {
+            isAuthenticated ? (
+              <div className= "google-home">
+             
+             <Home />
+             <button onClick={(e) => logout()} className='logout-button'>Logout</button>
+             </div>
+            ) : (
+      <div className="container">
+          <div className="welcome-text">
+              <h2>Welcome to Google Login Page</h2>
+              <p>Login and Access the data</p>
+          </div>
+          <button onClick = {(e) => loginWithRedirect()} className="login-button">
+                Login
+              </button>
+      </div>  
+            )}
+      </div>
+    );
+  }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
+import { BrowserRouter } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
+    <Auth0Provider
+    domain="dev-5is1g0kd3yowp74i.us.auth0.com"
+    clientId="3AXKH60ggEGbV1pfk8PE2uACbk2USijL"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+    >
     <App />
+    </Auth0Provider>
+    </BrowserRouter>
+    
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
